a947bae3ccacd6cd1efe04cab0c7b7de7911842d
SunDawning <dpmeichen@gmail.com>
2020-12-25 20:06:50 +0800

    修复 中文名称出错

--- DrcobjExporter.module.js
+++ DrcobjExporter.module.js
@@ -108,16 +108,43 @@ DrcobjExporter.prototype={
         return drcGeometries;
 
     }
-    
+
 };
 
 function stringToByteArray(str) {
+
+    // javascript中string转UTF8格式byte数组_第三眼的思绪-CSDN博客: https://blog.csdn.net/a123638/article/details/46535239
+    function str2UTF8(str){
+        var bytes = new Array(); 
+        var len,c;
+        len = str.length;
+        for(var i = 0; i < len; i++){
+	    c = str.charCodeAt(i);
+	    if(c >= 0x010000 && c <= 0x10FFFF){
+	        bytes.push(((c >> 18) & 0x07) | 0xF0);
+	        bytes.push(((c >> 12) & 0x3F) | 0x80);
+	        bytes.push(((c >> 6) & 0x3F) | 0x80);
+	        bytes.push((c & 0x3F) | 0x80);
+	    }else if(c >= 0x000800 && c <= 0x00FFFF){
+	        bytes.push(((c >> 12) & 0x0F) | 0xE0);
+	        bytes.push(((c >> 6) & 0x3F) | 0x80);
+	        bytes.push((c & 0x3F) | 0x80);
+	    }else if(c >= 0x000080 && c <= 0x0007FF){
+	        bytes.push(((c >> 6) & 0x1F) | 0xC0);
+	        bytes.push((c & 0x3F) | 0x80);
+	    }else{
+	        bytes.push(c & 0xFF);
+	    }
+        }
+        return bytes;
+    }
+    str = str2UTF8(str);
 
     var buffer = new ArrayBuffer(str.length);
     var bufferView = new Uint8Array(buffer);
 
     for (var i = 0, strLen = str.length; i < strLen; i++) {
-        bufferView[i] = str.charCodeAt(i);
+        bufferView[i] = str[i];
     }
 
     return buffer;
