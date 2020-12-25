4b999bae295857cc35e354f9eab9d666f49bdc2a
SunDawning <dpmeichen@gmail.com>
2020-12-25 22:09:58 +0800

    优化 直接将Array转换为ArrayBuffer

--- DrcobjExporter.module.js
+++ DrcobjExporter.module.js
@@ -119,33 +119,27 @@ function stringToByteArray(str) {
         var len,c;
         len = str.length;
         for(var i = 0; i < len; i++){
-	    c = str.charCodeAt(i);
-	    if(c >= 0x010000 && c <= 0x10FFFF){
-	        bytes.push(((c >> 18) & 0x07) | 0xF0);
-	        bytes.push(((c >> 12) & 0x3F) | 0x80);
-	        bytes.push(((c >> 6) & 0x3F) | 0x80);
-	        bytes.push((c & 0x3F) | 0x80);
-	    }else if(c >= 0x000800 && c <= 0x00FFFF){
-	        bytes.push(((c >> 12) & 0x0F) | 0xE0);
-	        bytes.push(((c >> 6) & 0x3F) | 0x80);
-	        bytes.push((c & 0x3F) | 0x80);
-	    }else if(c >= 0x000080 && c <= 0x0007FF){
-	        bytes.push(((c >> 6) & 0x1F) | 0xC0);
-	        bytes.push((c & 0x3F) | 0x80);
-	    }else{
-	        bytes.push(c & 0xFF);
-	    }
+            c = str.charCodeAt(i);
+            if(c >= 0x010000 && c <= 0x10FFFF){
+                bytes.push(((c >> 18) & 0x07) | 0xF0);
+                bytes.push(((c >> 12) & 0x3F) | 0x80);
+                bytes.push(((c >> 6) & 0x3F) | 0x80);
+                bytes.push((c & 0x3F) | 0x80);
+            }else if(c >= 0x000800 && c <= 0x00FFFF){
+                bytes.push(((c >> 12) & 0x0F) | 0xE0);
+                bytes.push(((c >> 6) & 0x3F) | 0x80);
+                bytes.push((c & 0x3F) | 0x80);
+            }else if(c >= 0x000080 && c <= 0x0007FF){
+                bytes.push(((c >> 6) & 0x1F) | 0xC0);
+                bytes.push((c & 0x3F) | 0x80);
+            }else{
+                bytes.push(c & 0xFF);
+            }
         }
         return bytes;
     }
-    str = str2UTF8(str);
 
-    var buffer = new ArrayBuffer(str.length);
-    var bufferView = new Uint8Array(buffer);
-
-    for (var i = 0, strLen = str.length; i < strLen; i++) {
-        bufferView[i] = str[i];
-    }
+    var buffer = new Uint8Array(str2UTF8(str)).buffer;
 
     return buffer;
 
